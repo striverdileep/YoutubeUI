5ca8ebc3df4ada03f78df5b0e518ed3cc97e3134
Dileep <striverdileep@gmail.com>
2025-12-29 19:24:52 +0530

    Update AppBar

--- src/components/YouTubeHeader.js
+++ src/components/YouTubeHeader.js
@@ -1,119 +1,109 @@
-import React from "react";
-import "./YouTubeHeader.css";
 import {
-  Avatar,
-  Badge,
-  Box,
+  AppBar,
+  Button,
+  Icon,
   IconButton,
   TextField,
-  Tooltip,
+  Toolbar,
   Typography,
+  Box,
+  InputAdornment,
+  Badge,
+  Avatar,
+  Tooltip,
 } from "@mui/material";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import HomeIcon from "@mui/icons-material/Home";
+import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-import { InputAdornment } from "@mui/material";
+import YouTubeLogo from "../assets/YoutubeLogo.png";
 import SearchIcon from "@mui/icons-material/Search";
-import MicIcon from "@mui/icons-material/Mic";
 import AddIcon from "@mui/icons-material/Add";
-import { Stack } from "@mui/material";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import {Drawer} from "@mui/material"
-import { useState } from "react";
-import { grey } from "@mui/material/colors";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MicNoneIcon from "@mui/icons-material/MicNone";
 const YouTubeHeader = () => {
-  const [openDrawer, setopenDrawer] = useState(false);
   return (
-    <div className="header">
-      <Drawer
-        open={openDrawer}
-        onClose={() => {
-          setopenDrawer(false);
-        }}
+    <div>
+      <AppBar
+        sx={{ bgcolor: "white", width: "100%", height: "60px" }}
+        position="fixed"
       >
-        <Box width="150px" p={3}>
-          <IconButton>
-            <div style={{display:"flex"}}>
-              <HomeIcon/>
-              <Typography>Home</Typography>
-            </div>
-          </IconButton>
-        </Box>
-      </Drawer>
-      <div
-        style={{
-          alignSelf: "center",
-        }}
-      >
-        <IconButton
-          sx={{ marginLeft: "20px" }}
-          onClick={() => {
-            setopenDrawer(true);
-            console.log(openDrawer);
-          }}
-        >
-          <MenuIcon color="error.main" />
-        </IconButton>
-        <IconButton size="large">
-          <YouTubeIcon color="red" />
-          <Typography variant="h6">YouTube</Typography>
-        </IconButton>
-      </div>
-
-      <div
-        style={{
-          flexGrow: 1,
-          maxWidth: "1200px",
-        }}
-      >
-        <TextField
-          placeholder="search"
-          sx={{
-            flexGrow: 1,
-            borderRadius: "24px",
-            maxWidth: "700px",
-            width: "700px",
-            alignSelf: "center",
-          }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        ></TextField>
-
-        <Tooltip title="Search with your Voice">
+        <Toolbar>
           <IconButton
-            size="large"
+            disableRipple
             sx={{
               "&:hover": {
-                backgroundColor: grey,
+                backgroundColor: "rgba(222, 219, 219, 1)",
               },
             }}
           >
-            <MicIcon />
+            <MenuIcon />
           </IconButton>
-        </Tooltip>
-      </div>
-
-      <Stack direction="row" spacing={2} alignSelf="center">
-        <IconButton edge="start">
-          <AddIcon color="action" />
-          <Typography>Create</Typography>
-        </IconButton>
-        <Badge badgeContent={5} color="primary">
-          <IconButton size="large">
-            <NotificationsIcon />
+          <Button href="/">
+            <img src={YouTubeLogo} />
+          </Button>
+          <Box sx={{ flexGrow: 1 }}>
+            <TextField
+              fullWidth
+              size="small"
+              placeholder="Search"
+              variant="outlined"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "24px",
+                },
+                maxWidth: "550px",
+              }}
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+          </Box>
+          <Tooltip title="Search with your Voice">
+            <IconButton
+              size="large"
+              sx={{
+                "&:hover": {
+                  backgroundColor: "rgba(222, 219, 219, 1)",
+                },
+              }}
+            >
+              <MicNoneIcon />
+            </IconButton>
+          </Tooltip>
+          <IconButton
+            color="rgba(0, 0, 0, 1)"
+            sx={{
+              "&:hover": {
+                backgroundColor: "rgba(222, 219, 219, 1)",
+                borderRadius: "30px",
+              },
+            }}
+          >
+            <AddIcon fontSize="large" />
+            <Typography
+              variant="h6"
+              color="rgba(0, 0, 0, 1)"
+              fontSize="small"
+              fontStyle="bold"
+            >
+              Create
+            </Typography>
           </IconButton>
-        </Badge>
-        <IconButton sx={{ alignSelf: "center" }}>
-          <Avatar sx={{ bgcolor: "success.light" }}>D</Avatar>
-        </IconButton>
-      </Stack>
+          <IconButton>
+            <Badge badgeContent={5} color="primary">
+              <NotificationsIcon fontSize="large" />
+            </Badge>
+          </IconButton>
+          <IconButton>
+            <Avatar sx={{ bgcolor: "success.main" }}>D</Avatar>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
