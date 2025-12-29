a99bc2e856a7e96444711279fd822994e825e9b2
Dileep <striverdileep@gmail.com>
2025-12-29 21:34:24 +0530

    Upgraded App Bar

--- src/components/YouTubeHeader.js
+++ src/components/YouTubeHeader.js
@@ -11,23 +11,60 @@ import {
   Badge,
   Avatar,
   Tooltip,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import YouTubeLogo from "../assets/YoutubeLogo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import AddIcon from "@mui/icons-material/Add";
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import MicNoneIcon from "@mui/icons-material/MicNone";
+import HomeIcon from "@mui/icons-material/Home";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 const YouTubeHeader = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
+      <Drawer
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <Box width="150px" padding={3} paddingTop={1}>
+          <List>
+            <ListItem>
+              <Button
+                color="inherit"
+                sx={{
+                  width: "150px",
+                }}
+              >
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText sx={{textAlign:"left"}}>Home</ListItemText>
+              </Button>
+            </ListItem>
+            <ListItem></ListItem>
+          </List>
+        </Box>
+      </Drawer>
       <AppBar
         sx={{ bgcolor: "white", width: "100%", height: "60px" }}
         position="fixed"
       >
         <Toolbar>
           <IconButton
+            onClick={() => {
+              setOpen(true);
+            }}
             disableRipple
             sx={{
               "&:hover": {
